d7fb8d4596a15f9a3290e8a5e829786f6d4a4010
paulyo19 <catkillercleary@gmail.com>
2019-10-13 16:31:49 +0100

    editind the date section of this project

--- script.js
+++ script.js
@@ -40,5 +40,5 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 9000); // Change image every 2 seconds
+  setTimeout(showSlides, 9000); // Change image every 9 seconds
 }
